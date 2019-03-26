2328295ab7732f5ca67458ba34a1a54e8c3bba65
Tomas <mazurkevic.tomas@gmail.com>
2019-03-26 18:03:31 +0000

    Started doing draw bot

--- bot/src/bot/MyBot.java
+++ bot/src/bot/MyBot.java
@@ -1,6 +1,8 @@
 package bot;
 
+import ai.abstraction.AbstractAction;
 import ai.abstraction.AbstractionLayerAI;
+import ai.abstraction.Harvest;
 import ai.abstraction.pathfinding.AStarPathFinding;
 import ai.core.AI;
 import ai.core.ParameterSpecification;
@@ -12,6 +14,7 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import rts.*;
 import rts.units.Unit;
@@ -21,11 +24,21 @@ import rts.units.UnitTypeTable;
 public class MyBot extends AbstractionLayerAI {    
     private UnitTypeTable utt;
     private UnitType worker;
+    private UnitType archer;
+    private UnitType light;
+    private UnitType heavy;
+    private UnitType barracks;
+    private UnitType base;
     
     public MyBot(UnitTypeTable utt) {
         super(new AStarPathFinding());
         this.utt = utt;
         worker = utt.getUnitType("Worker");
+        archer = utt.getUnitType("Archer");
+        light = utt.getUnitType("Light");
+        heavy = utt.getUnitType("Heavy");
+        barracks = utt.getUnitType("Barracks");
+        base = utt.getUnitType("Base");
     }
     
 
@@ -44,8 +57,14 @@ public class MyBot extends AbstractionLayerAI {
     public PlayerAction getAction(int player, GameState gs) {
         PhysicalGameState pgs = gs.getPhysicalGameState();
         
+        // Harvest worker b ehaviour
         for (Unit unit : pgs.getUnits()) {
             // TODO: issue commands to units
+        	if(unit.getPlayer() == player && unit.getType().canHarvest)
+        	{
+        		
+        	}
+        	
         }
         
         return translateActions(player, gs);
@@ -55,4 +74,87 @@ public class MyBot extends AbstractionLayerAI {
     public List<ParameterSpecification> getParameters() {
         return new ArrayList<>();
     }
+    
+    public void WorkerHarvest(List<Unit> workers, Player p, PhysicalGameState pgs)
+    {
+    	int nBases = 0;
+    	int nBarracks = 0;
+    	
+    	List<Unit> freeWorkers = new LinkedList<Unit>();
+    	freeWorkers.addAll(workers);
+    	
+    	if (workers.isEmpty())
+    	{
+    		return;
+    	}
+    	
+    	for (Unit u1 : pgs.getUnits())
+    	{    		
+    		if(u1.getType() == base && u1.getPlayer() == p.getID())
+    		{
+    			nBases++;
+    		}
+    		
+    		if(u1.getType() == barracks && u1.getPlayer() == p.getID())
+    		{
+    			nBarracks++;
+    		}
+    	}
+    	
+    	List<Integer> reservedPositions = new LinkedList<Integer>();
+    	if (nBases == 0 && !freeWorkers.isEmpty())
+    	{
+    		if(p.getResources() >= base.cost)
+    		{
+    			Unit u = freeWorkers.remove(0);
+    			buildIfNotAlreadyBuilding(u, base, u.getX(), u.getY(), reservedPositions, p, pgs);
+    			
+    		}	
+    	}
+    	if (nBarracks == 0 && !freeWorkers.isEmpty())
+    	{
+    		if(p.getResources() >= barracks.cost)
+    		{
+    			Unit u = freeWorkers.remove(0);
+    			buildIfNotAlreadyBuilding(u, base, u.getX(), u.getY(), reservedPositions, p, pgs);
+    			
+    		}
+    	}
+    	
+    	// harvest with all the free workers:
+        for (Unit u : freeWorkers) {
+            Unit closestBase = null;
+            Unit closestResource = null;
+            int closestDistance = 0;
+            for (Unit u2 : pgs.getUnits()) {
+                if (u2.getType().isResource) {
+                    int d = Math.abs(u2.getX() - u.getX()) + Math.abs(u2.getY() - u.getY());
+                    if (closestResource == null || d < closestDistance) {
+                        closestResource = u2;
+                        closestDistance = d;
+                    }
+                }
+            }
+            closestDistance = 0;
+            for (Unit u2 : pgs.getUnits()) {
+                if (u2.getType().isStockpile && u2.getPlayer()==p.getID()) {
+                    int d = Math.abs(u2.getX() - u.getX()) + Math.abs(u2.getY() - u.getY());
+                    if (closestBase == null || d < closestDistance) {
+                        closestBase = u2;
+                        closestDistance = d;
+                    }
+                }
+            }
+            if (closestResource != null && closestBase != null) {
+                AbstractAction aa = getAbstractAction(u);
+                if (aa instanceof Harvest) {
+                    Harvest h_aa = (Harvest)aa;
+                    if (h_aa.getTarget() != closestResource || h_aa.getBase()!=closestBase) harvest(u, closestResource, closestBase);
+                } else {
+                    harvest(u, closestResource, closestBase);
+                }
+            }
+        }
+    }
+    
 }
