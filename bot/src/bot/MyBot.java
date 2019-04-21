50fc37475c81a999d9a5ed3bf511c05fa317c884
Tomas Mazurkevic <mazurkevic.tomas@gmail.com>
2019-04-21 22:32:10 +0300

    * Added all unit behaviors

--- bot/src/bot/MyBot.java
+++ bot/src/bot/MyBot.java
@@ -4,6 +4,7 @@ import ai.abstraction.AbstractAction;
 import ai.abstraction.AbstractionLayerAI;
 import ai.abstraction.Harvest;
 import ai.abstraction.pathfinding.AStarPathFinding;
+import ai.abstraction.pathfinding.PathFinding;
 import ai.core.AI;
 import ai.core.ParameterSpecification;
 
@@ -56,20 +57,55 @@ public class MyBot extends AbstractionLayerAI {
     @Override
     public PlayerAction getAction(int player, GameState gs) {
         PhysicalGameState pgs = gs.getPhysicalGameState();
+        Player p = gs.getPlayer(player);
         
-        // Harvest worker behaviour
-        for (Unit unit : pgs.getUnits()) {
-            // TODO: issue commands to units
-        	if(unit.getPlayer() == player && unit.getType().canHarvest)
+        List<Unit> workers = new LinkedList<Unit>();
+        for (Unit unit : pgs.getUnits()) 
+        {
+        	// Base
+        	if(unit.getType() == base 
+        			&& unit.getPlayer() == player 
+        			&& gs.getActionAssignment(unit) == null)
         	{
-        		
+        		BaseBehavior(unit, p, pgs);
         	}
         	
+        	// Barracks
+        	if(unit.getType() == barracks 
+        			&& unit.getPlayer() == player 
+        			&& gs.getActionAssignment(unit) == null)
+        	{
+        		BarracksBehavior(unit, p, pgs);
+        	}
+        	
+        	// Melee
+        	if(unit.getType() == light || unit.getType() == heavy 
+        			&& unit.getPlayer() == player 
+        			&& gs.getActionAssignment(unit) == null)
+        	{
+        		MeleeBehavior(unit, p, gs);
+        	}
+        	
+        	// Ranged
+        	if(unit.getType() == archer 
+        			&& unit.getPlayer() == player 
+        			&& gs.getActionAssignment(unit) == null)
+        	{
+        		RangedBehavior(unit, p, gs);
+        	}
+        	
+        	// Workers
+        	if(unit.getPlayer() == player && unit.getType().canHarvest)
+        	{
+        		workers.add(unit);
+        	}
         }
+        WorkerBehavior(workers, p, pgs);
         
         return translateActions(player, gs);
     }
     
+    // Get the closest enemy
     public Unit GetClosestEnemy(PhysicalGameState pgs, Player p, Unit u)
     {
     	Unit closestEnemy = null;
@@ -89,15 +125,101 @@ public class MyBot extends AbstractionLayerAI {
     	return closestEnemy;
     }
     
-    @Override
-    public List<ParameterSpecification> getParameters() {
-        return new ArrayList<>();
+    // Base behavior
+    public void BaseBehavior(Unit u, Player p, PhysicalGameState pgs)
+    {
+    	int nWorkers = 0;
+    	for(Unit u2 : pgs.getUnits())
+    	{
+    		if(u2.getType() == worker && u2.getPlayer() == p.getID())
+    		{
+    			nWorkers++;
+    		}
+    	}
+    	
+    	// Train up to 3 workers
+    	if(nWorkers < 3 && p.getResources() >= worker.cost)
+    	{
+    		train(u, worker);
+    	}
     }
     
-    public void WorkerHarvest(List<Unit> workers, Player p, PhysicalGameState pgs)
+    // Barracks behavior (TO:DO - make a proper behavior for this)
+    public void BarracksBehavior(Unit u, Player p, PhysicalGameState pgs)
+    {
+    	if (p.getResources() >= archer.cost)
+    	{
+    		train(u, archer);
+    	}
+    }
+    
+    
+    // Melee unit behavior
+    public void MeleeBehavior(Unit u, Player p, GameState gs)
+    {
+    	PhysicalGameState pgs = gs.getPhysicalGameState();
+    	Unit closestEnemy = GetClosestEnemy(pgs, p, u);
+    	if(closestEnemy != null)
+    	{
+    		// Light Unit attack behavior
+    		if (u.getType() == light)
+    		{
+    			if(closestEnemy.getType() != heavy)
+    			{
+    				attack(u, closestEnemy);
+    			}
+    			else
+    			{
+    				int distanceX = Math.abs(u.getX() - closestEnemy.getX());
+            		int distanceY = Math.abs(u.getY() - closestEnemy.getY());
+            		move(u, distanceX, distanceY);
+    			}
+    		}
+    		else if (u.getType() == heavy)
+    		{
+    			if(closestEnemy.getType() != archer)
+    			{
+    				attack(u, closestEnemy);
+    			}
+    			else
+    			{
+    				int distanceX = Math.abs(u.getX() - closestEnemy.getX());
+            		int distanceY = Math.abs(u.getY() - closestEnemy.getY());
+            		move(u, distanceX, distanceY);
+    			}
+    		}
+    	}
+    }
+    
+    // Ranged unit behavior
+    public void RangedBehavior(Unit u, Player p, GameState gs)
+    {
+    	PhysicalGameState pgs = gs.getPhysicalGameState();
+    	Unit closestEnemy = GetClosestEnemy(pgs, p, u);
+    	if (closestEnemy != null)
+    	{
+    		if(closestEnemy.getType() != light)
+    		{
+    			// Attack the closes unit if it's not a Heavy
+    			attack(u, closestEnemy);
+    		}
+    		else
+        	{
+        		// Hopefully move from Heavy units
+        		int distanceX = Math.abs(u.getX() - closestEnemy.getX());
+        		int distanceY = Math.abs(u.getY() - closestEnemy.getY());
+        		move(u, distanceX, distanceY);
+        	}
+    	}
+    }
+    
+    // Worker behavior ? 
+    public void WorkerBehavior(List<Unit> workers, Player p, PhysicalGameState pgs)
     {
     	int nBases = 0;
     	int nBarracks = 0;
+    	
+    	int resourcesUsed = 0;
     	
     	List<Unit> freeWorkers = new LinkedList<Unit>();
     	freeWorkers.addAll(workers);
@@ -123,20 +245,20 @@ public class MyBot extends AbstractionLayerAI {
     	List<Integer> reservedPositions = new LinkedList<Integer>();
     	if (nBases == 0 && !freeWorkers.isEmpty())
     	{
-    		if(p.getResources() >= base.cost)
+    		if(p.getResources() >= base.cost + resourcesUsed)
     		{
     			Unit u = freeWorkers.remove(0);
     			buildIfNotAlreadyBuilding(u, base, u.getX(), u.getY(), reservedPositions, p, pgs);
-    			
+    			resourcesUsed += base.cost;
     		}	
     	}
     	if (nBarracks == 0 && !freeWorkers.isEmpty())
     	{
-    		if(p.getResources() >= barracks.cost)
+    		if(p.getResources() >= barracks.cost + resourcesUsed)
     		{
     			Unit u = freeWorkers.remove(0);
     			buildIfNotAlreadyBuilding(u, base, u.getX(), u.getY(), reservedPositions, p, pgs);
-    			
+    			resourcesUsed += barracks.cost;
     		}
     	}
     	
@@ -145,35 +267,69 @@ public class MyBot extends AbstractionLayerAI {
             Unit closestBase = null;
             Unit closestResource = null;
             int closestDistance = 0;
-            for (Unit u2 : pgs.getUnits()) {
-                if (u2.getType().isResource) {
+            for (Unit u2 : pgs.getUnits()) 
+            {
+                if (u2.getType().isResource) 
+                {
                     int d = Math.abs(u2.getX() - u.getX()) + Math.abs(u2.getY() - u.getY());
-                    if (closestResource == null || d < closestDistance) {
+                    if (closestResource == null || d < closestDistance) 
+                    {
                         closestResource = u2;
                         closestDistance = d;
                     }
                 }
             }
             closestDistance = 0;
-            for (Unit u2 : pgs.getUnits()) {
-                if (u2.getType().isStockpile && u2.getPlayer()==p.getID()) {
+            for (Unit u2 : pgs.getUnits()) 
+            {
+                if (u2.getType().isStockpile && u2.getPlayer()==p.getID()) 
+                {
                     int d = Math.abs(u2.getX() - u.getX()) + Math.abs(u2.getY() - u.getY());
-                    if (closestBase == null || d < closestDistance) {
+                    if (closestBase == null || d < closestDistance) 
+                    {
                         closestBase = u2;
                         closestDistance = d;
                     }
                 }
             }
-            if (closestResource != null && closestBase != null) {
+            if (closestResource != null && closestBase != null) 
+            {
                 AbstractAction aa = getAbstractAction(u);
-                if (aa instanceof Harvest) {
+                if (aa instanceof Harvest) 
+                {
                     Harvest h_aa = (Harvest)aa;
                     if (h_aa.getTarget() != closestResource || h_aa.getBase()!=closestBase) harvest(u, closestResource, closestBase);
-                } else {
+                } 
+                else 
+                {
                     harvest(u, closestResource, closestBase);
                 }
             }
         }
+        
+        // Command workers to attack if the enemy is close to them (Hopefully)
+        //for (Unit u : workers)
+        //{
+        //	Unit closestEnemy = GetClosestEnemy(pgs, p ,u);
+        //	int distance = Math.abs(closestEnemy.getX() - u.getX()) + Math.abs(closestEnemy.getY() - u.getY());
+        //	if (u.getHarvestAmount() == 0)
+        //	{
+        //		if (distance < 5)
+        //		{
+        //			attack(u, closestEnemy);
+        //		}
+        //	}
+        //}
+    }
+    
+    @Override
+    public List<ParameterSpecification> getParameters() 
+    {
+        List<ParameterSpecification> parameters = new ArrayList<>();
+        
+        parameters.add(new ParameterSpecification("PathFinding", PathFinding.class, new AStarPathFinding()));
+        
+        return parameters;
     }
     
 }
